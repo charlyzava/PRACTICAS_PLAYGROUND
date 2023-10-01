40b2337b96681185e0fc19d7ffc08d8237bc0f16
Carlos Zavaleta <charly_zava@yahoo.com.ar>
2023-09-30 23:20:39 -0300

    casi terminado

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//*****************************************************************************/
+//                        Alfabetización Digital III
+//*****************************************************************************/
+
+
 /*function invertirPalabra(palabra){
     nuevaPalabra = "";
     for (i=palabra.length - 1; i != -1 ; ) {
@@ -18,13 +23,42 @@ function primeraPalabra(oracion){
     }
 }
 console.log(primeraPalabra("La vida pasa rápido, disfrútala"));*/
-function ultimaPalabra(oracion){
+
+//Funciónn que fallaba en devolver la última palabra
+/*function ultimaPalabra(oracion){
     let palabra = "";
     let array = [];
     for (i=0; i < oracion.length; i++){
         if (oracion[i]===" "){
-
+           array.push(palabra);
+            palabra="";
+        } else {
+        palabra +=oracion[i];
         }
     }
+    array.push(palabra);
+    //return palabra;
+    return array[array.length -1];
+}*/
+
+/*
+function ultimaPalabra(oracion){
+    let array = oracion.split(" ");
+    return array[array.length -1];
+}*/
+//console.log(ultimaPalabra("Hay que ser feliz y disfrutar la vida!"));
+/*
+function listaDePalabras(oracion){
+    let lista = '["';
+    for ( i=0; i < oracion.length; i++){
+        if (oracion[i]===" "){
+            lista+= '","';
+        } else {
+            lista+=oracion[i];
+        }
+        
+    }
+    return lista;
 }
-//otra cosa
+console.log(listaDePalabras("Hay que disfrutar la vida y ser feliz!"));*/
+//Palabra Mâs Larga
