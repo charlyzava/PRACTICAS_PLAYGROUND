3d43da839b52c5a4b79fd2cebb432f6bfbf1e805
Carlos Zavaleta <charly_zava@yahoo.com.ar>
2023-09-30 22:03:38 -0300

    Prueba primer commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*function invertirPalabra(palabra){
+    nuevaPalabra = "";
+    for (i=palabra.length - 1; i != -1 ; ) {
+        nuevaPalabra=nuevaPalabra + palabra[i];
+        i--;
+    }
+    return nuevaPalabra;
+}
+console.log(invertirPalabra("Mi Zapallo es grande"));*/
+/*
+function primeraPalabra(oracion){
+    let primeraPalabra = "";
+    for (i=0; i < oracion.length; i++){
+        if(oracion[i]===" "){
+            return primeraPalabra + 5;
+        }
+        primeraPalabra+=oracion[i];
+    }
+}
+console.log(primeraPalabra("La vida pasa rápido, disfrútala"));*/
+function ultimaPalabra(oracion){
+    
+}
