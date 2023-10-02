11ff412a9e0a6b03335ade7402d7a42d8bb2eb06
Carlos Zavaleta <charly_zava@yahoo.com.ar>
2023-10-02 18:55:46 -0300

    modificacion de prueba

--- index.js
+++ index.js
@@ -62,3 +62,58 @@ function listaDePalabras(oracion){
 }
 console.log(listaDePalabras("Hay que disfrutar la vida y ser feliz!"));*/
 //Palabra Mâs Larga
+
+/*function palabraMasLarga(oracion){
+    let palabra ="";
+    let palabraLarga ="";
+    for ( i=0 ; i < oracion.length ; i++ ){
+
+        if (oracion[i] == " " ){
+            if (palabra.length >= palabraLarga.length){
+                palabraLarga = palabra;
+            }
+            palabra="";
+        } else {
+            palabra=palabra + oracion[i];
+        }
+        
+    }
+    if (palabra.length >= palabraLarga.length){
+        palabraLarga = palabra;
+    }
+    return palabraLarga;
+}*/
+/* *******************************************************************************************************
+Éste es el ejercicio resuelto, pero no contempla la última palabra si la frase con finaliza con un espacio
+**********************************************************************************************************
+function PalabraMásLarga(oración) {
+    //Obtengo el largo de la oración para poder recorrerla
+    const largo = oración.length;
+    //Inicializo la variable que contendrá la palabra
+    // más larga
+    let palabraMásLarga = "";
+    //Inicializo la variable que contendrá la palabra
+    // que voy leyendo
+    let palabraActual = "";
+    //Ciclo para recorrer cada posición de la oración
+    // i va de 0 a el largo-1
+    for (let i = 0; i < largo; i++) {
+    //Obtengo el caracter en la posición i del texto
+    const caracter = oración[i];
+    //Si el caracter es un espacio
+    if (caracter == " ") {
+    if (palabraActual.length >= palabraMásLarga.length) {
+    palabraMásLarga = palabraActual;
+    }
+    //Reinicio la palabra
+    palabraActual = "";
+    } else {
+    //Agrego el caracter a palabra ya que es
+    // parte de la palabra
+    palabraActual += caracter;
+    }
+    }
+    //Devuelvo la palabra más larga
+    return palabraMásLarga;
+    }*/
+console.log(palabraMasLarga("Hay que disfrutar la vida y ser felizzzzzz!"));
